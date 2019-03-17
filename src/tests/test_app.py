f8949b6384636fe2b5899aa49f42655112bc7709
zongying_cao <zongying_cao@163.com>
2019-03-17 13:39:40 +0800

    #1: finish the testing
    Signed-off-by: zongying_cao <zongying_cao@163.com>

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -10,9 +10,16 @@ init_test_folder()
 def load_for_get_data_file():
     load_resource('get_data_file')
 
+def load_for_get_data_file_error_case():
+    load_resource('get_data_file_error_case')
+    
 @with_setup(load_for_get_data_file, remove_test_folder)
 def test_get_data_file():
     assert_that(get_data_file(os.path.join(test_root(), 'get_data_file'))).is_equal_to(os.path.join(test_root(), 'get_data_file', 'data.json'))
-    
+
+@with_setup(load_for_get_data_file_error_case, remove_test_folder)
+def test_get_data_file_will_cause_error():
+    assert_that(get_data_file).raises(AssertionError).when_called_with(os.path.join(test_root(), 'get_data_file_error_case'))
+
 def test_get_collection_name():
     assert_that(get_collection_name('/data/hello.json')).is_equal_to('hello')    
